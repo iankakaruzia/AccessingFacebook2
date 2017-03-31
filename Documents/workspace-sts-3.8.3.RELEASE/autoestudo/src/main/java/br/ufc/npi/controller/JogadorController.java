63ed9ecb7f8ae3e5b6ae3b183566c6d140e77398
Ianka <Ianka@172.18.101.123>
2017-03-31 09:32:56 -0300

    AutoestudoNPI v1

---
+++ Documents/workspace-sts-3.8.3.RELEASE/autoestudo/src/main/java/br/ufc/npi/controller/JogadorController.java
@@ -0,0 +1,35 @@
+package br.ufc.npi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.ufc.npi.service.JogadorService;
+
+@Controller
+@RequestMapping(path="/jogadores/")
+public class JogadorController {
+	
+	@Autowired
+	JogadorService  service;
+	
+	@RequestMapping(path="/")
+	public ModelAndView index(){
+		ModelAndView model = new ModelAndView("jogadores");
+		
+		model.addObject("jogadores", service.getTodosJogadores());
+		
+		return model;
+	}
+	
+	@RequestMapping(path="/salvar", method=RequestMethod.POST)
+	public String salva(@RequestParam String nome, @RequestParam Integer idade){
+		service.salvarJogador(nome, idade);
+		
+		return "redirect:/jogadores/";
+	}
+
+}
---
+++ Documents/workspace-sts-3.8.3.RELEASE/autoestudo/src/main/java/br/ufc/npi/service/TimeService.java
@@ -0,0 +1,79 @@
+package br.ufc.npi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ufc.npi.bean.Jogador;
+import br.ufc.npi.bean.Time;
+import br.ufc.npi.repositorio.JogadorRepositorio;
+import br.ufc.npi.repositorio.TimeRepositorio;
+
+@Service
+public class TimeService {
+	
+	@Autowired
+	TimeRepositorio timeRepo;
+	
+	@Autowired
+	JogadorRepositorio jogadorRepo;
+	
+	
+	public Time salvarTime(String nome){
+		Time time = new Time();
+		time.setNome(nome);
+		
+		timeRepo.save(time);
+		
+		return time;
+	}
+	
+	public List<Time> getTodosTimes(){
+		return timeRepo.findAll();
+	}
+	
+	public Time getTime(Integer id){
+		Time time = timeRepo.findOne(id);
+		
+		return time;
+	}
+	
+	public boolean addJogadorAoTime(Integer idTime, Integer jogadorSemTime){
+		Time time = timeRepo.findOne(idTime);
+		
+		if(time.getJogadores().size() > 7){
+			return false;
+		}else{
+		
+			Jogador jogador = jogadorRepo.findOne(jogadorSemTime);
+		
+			time.getJogadores().add(jogador);
+			jogador.setTime(time);
+		
+			timeRepo.save(time);
+			jogadorRepo.save(jogador);
+			return true;
+		}
+	}
+	
+	public void delJogadorDoTime(Integer idTime, Integer idJogador){
+		Time time = timeRepo.findOne(idTime);
+		Jogador jogador = jogadorRepo.findOne(idJogador);
+		
+		time.getJogadores().remove(jogador);
+		jogador.setTime(null);
+		
+		timeRepo.save(time);
+		jogadorRepo.save(jogador);
+	}
+	
+	public void delTime(Integer idTime){
+		Time time = timeRepo.findOne(idTime);
+		Jogador jogador = new Jogador();
+		
+		//jojijoi
+	}
+	
+
+}
